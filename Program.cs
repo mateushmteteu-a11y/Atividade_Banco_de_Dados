c8078f836dc492cdc4360b696ee28082535b0c61
PedroFelipeLima <pedrofelipeferreiralimaaaaaa@gmail.com>
2026-09-16 13:45:21 -0300

    insert

--- Program.cs
+++ Program.cs
@@ -11,9 +11,11 @@ class Program
         while (continuar)
         {
             Console.Clear();
-            Console.WriteLine("=== SISTEMA CRUD DE USUÁRIOS ===");
-            Console.WriteLine("1 - Cadastrar Novo Usuário");
-            Console.WriteLine("2 - Listar Todos os Usuários");
+            Console.WriteLine("=== SISTEMA CRUD DE FILME ===");
+            Console.WriteLine("1 - Cadastrar filme");
+            Console.WriteLine("2 - Listar Todos os filmes");
+            Console.WriteLine("3 - Atulizar filme");
+            Console.WriteLine("4 - Deletar filme");
             Console.WriteLine("0 - Sair");
             Console.Write("Escolha uma opção: ");
             
@@ -43,4 +45,46 @@ class Program
                     break;
             }
         }
+    }
+    static void Cadastrar()
+    {
+        Console.Clear();
+        Console.WriteLine("--- NOVO CADASTRO ---");
+
+        Console.Write("Título do Filme: ");
+        string? exTitulo = Console.ReadLine();
+
+        Console.Write("Gênero: ");
+        string? exGenero = Console.ReadLine();
+
+        Console.Write("Data de Lançamento (DD/MM/AAAA): ");
+        DateTime exAno = DateTime.Parse(Console.ReadLine());
+
+        Filme novoFilme = new Filme(exTitulo, exGenero, exAno);
+
+        using (MySqlConnection conexao = new MySqlConnection(connectionString))
+        {
+            try
+            {
+                conexao.Open();
+                string sql = "INSERT INTO filme (titulo, genero, ano) VALUES (@titulo, @genero, @ano)";
+                
+                using (MySqlCommand comando = new MySqlCommand(sql, conexao))
+                {
+                    comando.Parameters.AddWithValue("@titulo", novoFilme.Titulo);
+                    comando.Parameters.AddWithValue("@genero", novoFilme.Genero);
+                    comando.Parameters.AddWithValue("@ano", novoFilme.Ano);
+
+                    comando.ExecuteNonQuery();
+                    Console.WriteLine($"\n{novoFilme.Titulo} foi cadastrado no MySQL.");
+                }
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine($"\nErro ao salvar: {ex.Message}");
+            }
+        }
+
+        Console.WriteLine("\nPressione qualquer tecla para voltar ao menu...");
+        Console.ReadKey();
     }}
