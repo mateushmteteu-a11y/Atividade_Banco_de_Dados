8ddd7d137178f0fa3bf14dbc0aca3ba644fdad26
Thomas Duarte <duartethomas2011@gmail.com>
2026-09-16 14:24:06 -0300

    arrumando erros

--- obj/Debug/net10.0/Atividade_Banco_de_Dados.AssemblyInfo.cs
+++ obj/Debug/net10.0/Atividade_Banco_de_Dados.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Atividade_Banco_de_Dados")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+c114a631a2f6706026ee9c7f7731965480e72ad7")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+319b820834b183f2288799ff4916d6a320632c66")]
 [assembly: System.Reflection.AssemblyProductAttribute("Atividade_Banco_de_Dados")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Atividade_Banco_de_Dados")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
