3a54592018341073bbcc7b7c66b6f148a0b2564d
PedroFelipeLima <pedrofelipeferreiralimaaaaaa@gmail.com>
2026-09-21 13:42:11 -0300

    Revert "Sem ronaldo nos arquivos"
    This reverts commit b71f7315f58e15a414ddf881c7b63737b4f4a532.

--- obj/Debug/net10.0/Atividade_Banco_de_Dados.AssemblyInfo.cs
+++ obj/Debug/net10.0/Atividade_Banco_de_Dados.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Atividade_Banco_de_Dados")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+a42f0eac0d19922c34446b0feca27f73535e0ab0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+6f776437e9115f420bde3f509f74fd8de7fdc084")]
 [assembly: System.Reflection.AssemblyProductAttribute("Atividade_Banco_de_Dados")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Atividade_Banco_de_Dados")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
