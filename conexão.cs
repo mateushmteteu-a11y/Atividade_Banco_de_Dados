a083275e5bada3311b8eba91acc92a3dce7e2d9c
Thomas Duarte <duartethomas2011@gmail.com>
2026-09-23 14:32:14 -0300

    apresentar

--- conexão.cs
+++ conexão.cs
@@ -2,7 +2,7 @@ using MySql.Data.MySqlClient;
 
 public class Conexao
 {
-    private string stringConexao = "Server=localhost;Database=sistema_filmes;Uid=root;Pwd=;";
+    private string stringConexao = "Server=localhost;Database=sistema_filmes;Uid=root;Pwd=;"; // serve para saber onde está o o banco e como entrar nele
 
     public MySqlConnection Conectar()
     {
