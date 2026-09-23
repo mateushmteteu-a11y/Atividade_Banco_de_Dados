2ac8166276c6e7c8d7871c5fb82c1277985b4c9e
MateusHM <mateushmteteu@gmail.com>
2026-09-23 13:04:56 -0300

    arrumei

--- README.md
+++ README.md
@@ -1,14 +1,23 @@
+Nome do projeto;
 # Atividade_Banco_de_Dados
+
+Integrantes;
 Alunos: Pedro Felipe, Thomas, Antônio e Mateus
 
+Banco de dados utilizado;
 O banco de dados utilizado foi o Mysql
 
+Biblioteca/driver utilizado;
 O driver usado foi o MysqlConnector
 
+Como instalar as dependências;
 Digitando "dotnet add package MySqlConnector" no terminal
 
+Como configurar o banco;
 Para o banco de dados copie do ronaldo.sql e cole no mysql
 
+Como executar o projeto;
 Para rodar o sistema aperte Ctrl+j e digite dotnet run
 
+Breve explicação de como funciona a conexão.
 Através da connectionString o sistema pega informações do banco para se conectar e assim consegue executar os códigos
--- obj/Debug/net10.0/Atividade_Banco_de_Dados.AssemblyInfo.cs
+++ obj/Debug/net10.0/Atividade_Banco_de_Dados.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Atividade_Banco_de_Dados")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+3a54592018341073bbcc7b7c66b6f148a0b2564d")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+71f4f734952f92f6c8e9fccccd7133ea0bcc0d4a")]
 [assembly: System.Reflection.AssemblyProductAttribute("Atividade_Banco_de_Dados")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Atividade_Banco_de_Dados")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
